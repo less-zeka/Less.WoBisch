98b326ad47add72e279fa33ed4aaed6ebb0eccb9
shahingut <shahin.gut@gmail.com>
2015-02-19 10:51:13 +0100

    Initial Version

---
+++ SignalRChat/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNet.SignalR;
+using System.Device.Location;
+
+namespace SignalRChat
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.broadcastMessage(name, message);
+        }
+
+        public void UpdatePosition(string name, double latitude, double longitude)
+        {
+            //var geocoordinate = new GeoCoordinate(10, 10);
+            Clients.All.updatePosition(name, latitude, longitude);
+        }
+    }
+}
